d83f77cba1ec5390f24164f1f93fe6a70f156e8d
tinolan <valentinlanger24@googlemail.com>
2020-11-06 15:56:26 +0100

    SumApi

--- app/src/main/java/com/example/kjg_automat/MainActivity.java
+++ app/src/main/java/com/example/kjg_automat/MainActivity.java
@@ -1,45 +1,50 @@
 package com.example.kjg_automat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Skeleton of an Android Things activity.
- * <p>
- * Android Things peripheral APIs are accessible through the PeripheralManager
- * For example, the snippet below will open a GPIO pin and set it to HIGH:
- * <p>
- * PeripheralManager manager = PeripheralManager.getInstance();
- * try {
- * Gpio gpio = manager.openGpio("BCM6");
- * gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
- * gpio.setValue(true);
- * } catch (IOException e) {
- * Log.e(TAG, "Unable to access GPIO");
- * }
- * <p>
- * You can find additional examples on GitHub: https://github.com/androidthings
- */
+import com.sumup.merchant.reader.models.TransactionInfo;
+import com.sumup.merchant.reader.api.SumUpAPI;
+import com.sumup.merchant.reader.api.SumUpLogin;
+import com.sumup.merchant.reader.api.SumUpPayment;
+
+
 public class MainActivity extends Activity {
+
+    private static final String TAG = "Automat-Aktivität";
+    private static final int REQUEST_CODE_LOGIN = 1;
+    private static final int REQUEST_CODE_PAYMENT = 2;
+    private static final int REQUEST_CODE_PAYMENT_SETTINGS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = (Button) findViewById(R.id.button_test);
-        button.setOnClickListener(new View.OnClickListener() {
+
+
+
+        final Button login = (Button) findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                button.setText("Button gedrückt");
+                login.setText("Button Login gedrückt, Login wird gestartet");
+                SumUpLogin automat_login = SumUpLogin.builder("155d4d1d-ca6c-4032-9020-2dec4ba1c8eb").build();
+                SumUpAPI.openLoginActivity(MainActivity.this, automat_login, REQUEST_CODE_LOGIN);
+
             }
 
             });
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
 }
